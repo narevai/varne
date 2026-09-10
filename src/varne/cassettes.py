be2215ccb1d83ca9da21cedaf93b9f841125f8b8
oskar <53711292+oskarkocol@users.noreply.github.com>
2026-09-10 10:24:19 +0000

    fix: pyright

--- src/varne/cassettes.py
+++ src/varne/cassettes.py
@@ -1,7 +1,9 @@
-from contextlib import ExitStack
+from contextlib import AbstractContextManager, ExitStack
 from pathlib import Path
+from typing import cast
 
 import vcr
+from vcr.cassette import Cassette
 
 CASSETTES = [
     Path("tests/unit/providers/jsonplaceholder/cassettes/test_client/test_fetch.yaml"),
@@ -10,17 +12,19 @@ CASSETTES = [
 
 class CassetteManager:
     def __init__(self, cassette_paths: list[Path]):
-        self.cassette_paths = cassette_paths
-        self._stack = ExitStack()
+        self.cassette_paths: list[Path] = cassette_paths
+        self.casette_stack: ExitStack = ExitStack()
 
     def start(self) -> None:
         for cassette_path in self.cassette_paths:
-            self._stack.enter_context(
+            cassette_cm = cast(
+                AbstractContextManager[Cassette],
                 vcr.use_cassette(
-                    str(cassette_path),
+                    str(cassette_path.resolve()),
                     record_mode="none",
-                )
+                ),
             )
+            self.casette_stack.enter_context(cassette_cm)
 
     def stop(self) -> None:
-        self._stack.close()
+        self.casette_stack.close()
