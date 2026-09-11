801332820128eb127555a8784893b7a1c0ba2d27
oskar <53711292+oskarkocol@users.noreply.github.com>
2026-09-11 04:36:22 +0000

    refactor: move policies to the test file

--- tests/unit/providers/vercel/test_client_billing.py
+++ tests/unit/providers/vercel/test_client_billing.py
@@ -3,11 +3,42 @@ from functools import partial
 
 import httpx2 as httpx
 import pytest
+from tests.policy import FieldPolicy, SanitizePolicy
 from tests.sanitize import sanitize_response
-from tests.unit.providers.vercel.policy import policy_vercel_billing
 
 from varne.config import VercelToken
 from varne.providers.vercel.client import VercelClient
+
+policy_vercel_billing = SanitizePolicy(
+    root=FieldPolicy(
+        match_field="Tags.ProjectId",
+        match_value="prj_5LAip7eW0S0iDBoLNDwa9gMTye78",
+        limit=10,
+        keep=(
+            "ChargePeriodStart",
+            "ChargePeriodEnd",
+            "ChargeCategory",
+            "BilledCost",
+            "BillingCurrency",
+            "EffectiveCost",
+            "ServiceName",
+            "ServiceCategory",
+            "ServiceProviderName",
+            "ConsumedQuantity",
+            "ConsumedUnit",
+            "PricingCategory",
+            "PricingCurrency",
+            "PricingQuantity",
+            "PricingUnit",
+        ),
+    ),
+    fields=(
+        FieldPolicy(
+            field="Tags",
+            keep=("ProjectId",),
+        ),
+    ),
+)
 
 
 @pytest.fixture
--- tests/unit/providers/vercel/test_client_projects.py
+++ tests/unit/providers/vercel/test_client_projects.py
@@ -2,11 +2,27 @@ from functools import partial
 
 import httpx2 as httpx
 import pytest
+from tests.policy import FieldPolicy, SanitizePolicy
 from tests.sanitize import sanitize_response
-from tests.unit.providers.vercel.policy import policy_vercel_projects
 
 from varne.config import VercelToken
 from varne.providers.vercel.client import VercelClient
+
+policy_vercel_projects = SanitizePolicy(
+    fields=(
+        FieldPolicy(
+            field="projects",
+            keep=("id", "accountId"),
+            limit=2,
+            match_field="id",
+            match_value="prj_5LAip7eW0S0iDBoLNDwa9gMTye78",
+        ),
+        FieldPolicy(
+            field="protectionBypass",
+            redact_keys=True,
+        ),
+    )
+)
 
 
 @pytest.fixture
