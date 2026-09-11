e02242b61bdceafb11fa4ee155ec5b38509a2def
oskar <53711292+oskarkocol@users.noreply.github.com>
2026-09-11 05:00:37 +0000

    feat: add vercel web analytics

--- src/varne/providers/vercel/client.py
+++ src/varne/providers/vercel/client.py
@@ -70,3 +70,24 @@ class VercelClient(ProviderClient):
         )
 
         return response.text
+
+    def fetch_web_analytics_visits_aggregate(
+        self,
+        team_id: str,
+        project_id: str,
+        date_from: datetime,
+        date_to: datetime,
+    ) -> str:
+        response = self.http.get(
+            url=f"{self.base_url}/v1/query/web-analytics/visits/aggregate",
+            headers=self.headers,
+            params={
+                "teamId": team_id,
+                "projectId": project_id,
+                "since": date_from.isoformat(),
+                "until": date_to.isoformat(),
+                "by": ["day"],
+            },
+        )
+
+        return response.text
--- tests/unit/providers/vercel/test_client_web_analytics_visits_aggregate.py
+++ tests/unit/providers/vercel/test_client_web_analytics_visits_aggregate.py
@@ -9,7 +9,7 @@ from tests.sanitize import sanitize_response
 from varne.config import VercelToken
 from varne.providers.vercel.client import VercelClient
 
-policy_vercel_web_analytics_visits_count = SanitizePolicy(
+policy_vercel_web_analytics_visits_aggregate = SanitizePolicy(
     root=FieldPolicy(
         keep=(
             "version",
@@ -26,13 +26,13 @@ def vcr_config():
         "decode_compressed_response": True,
         "filter_headers": ["authorization"],
         "before_record_response": partial(
-            sanitize_response, policy=policy_vercel_web_analytics_visits_count
+            sanitize_response, policy=policy_vercel_web_analytics_visits_aggregate
         ),
     }
 
 
 @pytest.mark.vcr
-def test_fetch_web_analytics_visits_count(
+def test_fetch_web_analytics_visits_aggregate(
     http_client: httpx.Client,
     vercel_token: VercelToken,
     vercel_team_id: str,
@@ -40,7 +40,7 @@ def test_fetch_web_analytics_visits_count(
 ):
     client = VercelClient(http_client, api_token=vercel_token)
 
-    web_analytics = client.fetch_web_analytics_visits_count(
+    web_analytics = client.fetch_web_analytics_visits_aggregate(
         team_id=vercel_team_id,
         project_id=vercel_project_id,
         date_from=datetime(2026, 9, 1, tzinfo=UTC),
